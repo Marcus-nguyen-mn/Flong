fb07923fd4081e671578a99cf2d5332c5455b8cb
Marcus <imkay2712@gmail.com>
2024-08-07 09:32:52 +0700

    remove code header and footer

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -5,12 +5,6 @@
 get_header();
 ?>
 <?php 
-    //get_template_part('sections/home/section-1'); 
+    get_template_part('sections/home/section-1'); 
 ?>
-    
-    
-    
-    
-    
-    
 <?php get_footer();?>
