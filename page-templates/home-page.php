ac08aafbd65cbea51d753d3717c2719457383cbe
Marcus <imkay2712@gmail.com>
2024-08-07 09:29:17 +0700

    push 2

--- page-templates/home-page.php
+++ page-templates/home-page.php
@@ -5,21 +5,8 @@
 get_header();
 ?>
 <?php 
-    get_template_part('sections/home/section-1'); 
+    //get_template_part('sections/home/section-1'); 
 ?>
-<div class="mc_para_main">
-    <?php 
-        get_template_part('sections/home/section-2'); 
-        get_template_part('sections/home/section-3'); 
-        get_template_part('sections/home/section-4'); 
-        get_template_part('sections/home/section-5'); 
-        get_template_part('sections/home/section-6'); 
-        get_template_part('sections/home/section-7');
-        get_template_part('sections/home/section-8');
-        get_template_part('sections/home/section-9');
-        get_template_part('sections/home/section-10');
-    ?>
-</div>
     
     
     
