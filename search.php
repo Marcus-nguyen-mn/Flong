d7d2bab3373e8938353e1344ff781e1c41c61c21
Marcus <imkay2712@gmail.com>
2024-08-12 14:59:24 +0700

    anything

--- search.php
+++ search.php
@@ -1,59 +1,115 @@
 <?php
 get_header();
 ?>
-<div class="mc-archive mc_search_page">
-    <div class="archive-banner-and-title">
-        <div class="banner">
-            <img src="<?php echo get_field("banner_projects_page","option"); ?>" alt="Banner">
-        </div>
-        <div></div>
-        <div class="mc-bread-and-title">
-            <div class="archive-main-title">
-                <?php if ( have_posts() ) : 
-                    $allsearch =new WP_Query("s=$s&showposts=-1");
-                    echo $allsearch ->found_posts." "; printf( __( 'Kết quả tìm kiếm cho từ khóa: %s', 'WebExp24h' ), '<span>' . esc_html( get_search_query() ));
-                ?>
-                <?php endif; ?>
+<?php get_template_part('sections/slide-gen'); ?>
+<section class="list-news-wrapper py-5">
+    <div class="container">
+        <div class="row mb-3 mb-lg-4">
+            <div class="col-12 mx-auto">
+                <h1 class="title-page"><?php echo "Kết quả tìm kiếm";?></h1>
             </div>
-            <div class="mc-bread">
-                <a href="<?php echo site_url(); ?>">Trang chủ</a>
-                <p><i class="fa-solid fa-angles-right"></i></p>
-                <div>Tìm kiếm</div>
-            </div>
-            
         </div>
-    </div>
-    <div class="archiv-pr-cover">
-        <div class="mc-container">
-            <div class="list-posts">
-                <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-                    <div class="item-post" data-aos="fade-up" data-aos-duration="700">
-                        <a href="<?php the_permalink(); ?>" class="thumb-post">
-                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php echo get_the_title(); ?>" />
-                        </a>
-                        <a href="<?php the_permalink(); ?>" class="title-and-arrow-one-projects">
-                            <div class="title-item">
-                                <?php the_title(); ?>
+        <?php if (have_posts()) : ?>
+            <div class="category-posts">
+                <div class="row mb-4">
+                    <?php
+                    $post_counter = 0;
+                    while (have_posts()) : the_post();
+                        $post_counter++;
+
+                        if ($post_counter == 1) :
+                            // Bài viết đầu tiên kiểu hiển thị A, col-6
+                            ?>
+                            <div class="col-12 col-lg-7 ms-auto mb-4 mb-lg-0">
+                                <div class="post post-first post-custom w-100 h-100 d-block">
+                                    <a href="<?php the_permalink();?>" class="d-block overflow-hidden position-relative w-100 h-100">
+                                        <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="post-thumbnail w-100 h-100 object-fit-cover d-block">
+                                        <div class="post-content">
+                                            <div class="d-flex align-items-end">
+                                                <div class="post-info pe-3">
+                                                    <h3 class="post-title"><?php the_title(); ?></h3>
+                                                    <div class="entry mb-0">
+                                                        <?php the_excerpt(); ?>
+                                                    </div>
+                                                </div>
+                                                <span class="d-flex justify-content-center align-items-center readmore-icon">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                                        <path d="M12.666 8L2.66602 8" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                        <path d="M10 4.66602L12.6262 7.29224C12.9596 7.62558 13.1262 7.79224 13.1262 7.99935C13.1262 8.20646 12.9596 8.37312 12.6262 8.70646L10 11.3327" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
-                            <div class="arrow-white">
-                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow.png" alt="Arrow">
+                            <div class="col-12 col-lg-5 me-auto">
+                                <div class="d-flex flex-column gap-4">
+                                    <?php elseif ($post_counter == 2 || $post_counter == 3) : ?>
+                                        <div class="post post-first">
+                                            <a href="<?php the_permalink();?>" class="d-block overflow-hidden position-relative w-100 h-100">
+                                                <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="post-thumbnail w-100 h-100 object-fit-cover d-block">
+                                                <div class="post-content">
+                                                    <div class="d-flex align-items-end">
+                                                        <div class="post-info pe-3">
+                                                            <h3 class="post-title"><?php the_title(); ?></h3>
+                                                            <div class="entry mb-0">
+                                                                <?php the_excerpt(); ?>
+                                                            </div>
+                                                        </div>
+                                                        <span class="d-flex justify-content-center align-items-center readmore-icon">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                                                                <path d="M12.666 8L2.66602 8" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                                <path d="M10 4.66602L12.6262 7.29224C12.9596 7.62558 13.1262 7.79224 13.1262 7.99935C13.1262 8.20646 12.9596 8.37312 12.6262 8.70646L10 11.3327" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                                            </svg>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    <?php if ($post_counter == 3) : ?>
+                                </div>
+                               
                             </div>
-                        </a>
-                        <div class="desc-item-project">
-                            <?php echo get_the_excerpt(); ?>
                         </div>
-                    </div>
-                <?php endwhile; endif; ?>
+                        <div class="row">
+                            <?php
+                        endif;
+                    else :
+                        // Các bài viết còn lại kiểu hiển thị B
+                        ?>
+                        <div class="col-12 mx-auto mb-4">
+                            <div class="post post-second d-flex flex-column flex-lg-row align-items-center gap-3">
+                                <div class="post-thumb overflow-hidden">
+                                    <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title(); ?>" class="w-100 h-100 object-fit-cover d-block">
+                                </div>
+                                <div class="post-content">
+                                    <div class="post-date mb-3">Tin đăng: <?php echo get_the_date('\N\g\à\y j \t\há\n\g n \n\ă\m Y')?></div>
+                                    <h3 class="post-title"><?php the_title(); ?></h3>
+                                    <div class="entry">
+                                        <?php the_excerpt(); ?>
+                                    </div>
+                                    <a href="<?php the_permalink();?>" class="btn-detail">Tiếp tục đọc</a>
+                                </div>
+                               
+                            </div>
+                        </div>
+                    <?php
+                    endif;
+                endwhile;
+                ?>
+                </div>
             </div>
-            <?php
-                the_posts_pagination( array(
-                'prev_text' => __( 'Trước', 'textdomain' ),
-                'next_text' => __( 'Sau', 'textdomain' ),
-                ) );
-            ?>
-        </div>
+            <div class="pagination-wrapper mt-4">
+                <?php  
+                    echo custom_pagination($wp_query);
+                ?>
+            </div>
+        <?php else : ?>
+            <p><?php _e('No posts found in this category.', 'textdomain'); ?></p>
+        <?php endif; ?>
     </div>
-</div>
+</section>
+<?php get_footer(); ?>
 
 
-<?php get_footer();?>
